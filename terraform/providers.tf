16328f56c7389fae926f7715917b2cc7b8d84f1a
pabl-o-ce <cye@poscye.com>
2023-04-02 02:31:13 -0500

    add all cloud providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,18 @@ terraform {
       source = "hashicorp/aws"
       version = "4.60.0"
     }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "4.0.0"
+    }
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.37.0"
+    }
   }
 }
 
@@ -12,6 +24,25 @@ provider "aws" {
   # Add AWS region
   region= var.aws_region
   # Add AWS keys
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+// Digital Ocean provider
+provider "do" {
+  # Add Digital Ocean token
+  token = var.do_token
+}
+
+// GCP provider
+provider "google" {
+  // Add GCP project
+  region  = var.gcp_region
+  project = var.gcp_project_id
+}
+
+// Hetzner provider
+provider "hcloud" {
+  // Add hcloud token
+  token = var.hetzner_token
 }
